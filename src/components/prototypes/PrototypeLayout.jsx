552dc61934030f3e34b0413d5b526e528f2452fc
Kevin Nathan <kevin.max.nathan@gmail.com>
2026-04-29 08:21:30 -0600

    grey to white

--- src/components/prototypes/PrototypeLayout.jsx
+++ src/components/prototypes/PrototypeLayout.jsx
@@ -28,7 +28,7 @@ const PrototypeLayout = () => {
   }
   
   return (
-    <div className="min-h-screen flex flex-col bg-gray-100">
+    <div className="min-h-screen flex flex-col bg-white">
       {/* Header with Back Button */}
       <div className="bg-white border-b border-gray-200 px-4 py-2 flex-shrink-0 sticky top-0 z-30">
         <div className="flex items-center justify-between">
