8f4811c42005cfd1b5a474c6e982052d60414912
Kevin Nathan <kevin.max.nathan@gmail.com>
2026-05-05 23:36:12 -0600

    Add debug logging to Jira proxy function

--- netlify/functions/jira.js
+++ netlify/functions/jira.js
@@ -1,23 +1,36 @@
 export const handler = async (event) => {
-  const jiraPath = event.path.replace('/api/jira', '')
-  const query = event.rawQuery ? `?${event.rawQuery}` : ''
+  // Debug: log what Netlify gives us so we can verify path extraction
+  const rawPath = event.rawUrl
+    ? new URL(event.rawUrl).pathname
+    : event.path
+
+  console.log('[jira] event.path    :', event.path)
+  console.log('[jira] event.rawUrl  :', event.rawUrl)
+  console.log('[jira] rawPath       :', rawPath)
+
+  const jiraPath = rawPath.replace(/^\/api\/jira/, '')
+  const query = event.rawUrl ? new URL(event.rawUrl).search : (event.rawQuery ? `?${event.rawQuery}` : '')
+  const targetUrl = `https://billingplatform.atlassian.net${jiraPath}${query}`
+
+  console.log('[jira] targetUrl     :', targetUrl)
+
   const token = Buffer.from(
     `${process.env.JIRA_EMAIL}:${process.env.JIRA_API_TOKEN}`
   ).toString('base64')
 
-  const response = await fetch(
-    `https://billingplatform.atlassian.net${jiraPath}${query}`,
-    {
-      method: event.httpMethod,
-      headers: {
-        Authorization: `Basic ${token}`,
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      ...(event.body ? { body: event.body } : {}),
-    }
-  )
+  const response = await fetch(targetUrl, {
+    method: event.httpMethod,
+    headers: {
+      Authorization: `Basic ${token}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    ...(event.body ? { body: event.body } : {}),
+  })
+
   const body = await response.text()
+  console.log('[jira] jira status   :', response.status)
+
   return {
     statusCode: response.status,
     headers: { 'Content-Type': 'application/json' },
